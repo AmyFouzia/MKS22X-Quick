1760b3467b83f05efd1f19a318c16a668bf1b0d5
Amy Fouzia <afouzia00@stuy.edu>
2019-03-11 14:50:51 -0400

    added partition

---
+++ Quick.java
@@ -0,0 +1,22 @@
+public class Quick{
+
+  /*Modify the array such that:
+ *1. Only the indices from start to end inclusive are considered in range
+ *2. A random index from start to end inclusive is chosen, the corresponding
+ *   element is designated the pivot element.
+ *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+ *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+ *@return the index of the final position of the pivot element.
+ */
+ int partition (int [] data, int start, int end){
+   int pivot = 0; //index of pivot
+
+   for(int i = 0; i < data.length - 1; i++){
+     start = data[i];
+     end = data[j];
+   }
+ }
+
+}
+
+// [0, 7, 9, 8, 5]

eeaf6ca82e75ca4c28c0c4aa53b18bf6910222ba
Amy Fouzia <afouzia00@stuy.edu>
2019-03-13 14:55:00 -0400

    end of class

--- Quick.java
+++ Quick.java
@@ -10,11 +10,38 @@ public class Quick{
  */
  int partition (int [] data, int start, int end){
    int pivot Math.random() * data.length; //index of pivot
-   start = data[0];
-   end = data[data.length - 1];
 
-//   while(pivot != (data.length - 1) / 2)
+   for(int i = 1; i < data.length - 1; i++){
+     data.add(data[pivot], 0);
+
+     start = data[i];
+     end = data[data.length-1];
+
+     if(start < data[pivot]){
+
+     }
+   }
+  }
+//  {0, 8, 7, 4, 9}
+/*return the value that is the kth smallest value of the array. k=0 is the smallest
+ */
+ public static int quickselect(int[] data, int k){
+
+ }
+
+/*Modify the array to be in increasing order.
+ */
+ public static void quicksort(int[] data){
+
+ }
+
+
+/*Things to do: (and the order that makes the most sense)
+1-Complete and test partition.
+2-Complete Quickselect using partition
+3-Improve partition (minimally using the easy improvement below) and test quickselect on an array of duplicate values. Should not be significantly longer than all unique values.
+4-Complete Quicksort.
+5-Optionally implement dutch flag paritioning and update quicksort to include this.
+*/
 
 }
-
-// [0, 7, 9, 8, 5, 4] --> 4

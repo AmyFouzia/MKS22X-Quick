00408ff2f7599c0c52b21d5cfb67abc5541b4c7b
fuadbinzia <afouzia00@stuy.edu>
2019-03-17 23:11:22 -0400

    added main to test, did qselect

--- Quick.java
+++ Quick.java
@@ -85,14 +85,26 @@ public class Quick{
  }
 
  private static int qsHelp(int[]data,int k, int start, int end){
-
+   int pivot = partition(data,start,end);
+   if(pivot < k) return quickselectH(data,k,pivot+1,end);
+   if(pivot == k) return data[pivot];
+   if(pivot > k) return quickselectH(data,k,start,pivot-1);
+   return -1;
  }
 
 /*Modify the array to be in increasing order.
  */
  public static void quicksort(int[] data){
-
+   quicksortHelp(data, 0, data.length-1);
  }
+
+ private static void quicksortHelp(int[] data2, int low, int high){
+  if(!(low > high)){
+    int pivot = partition(data2, low, high);
+    quicksort(data2, low, pivot-1);
+    quicksort(data2, pivot+1, high);
+  }
+}
 
 
 /*Things to do: (and the order that makes the most sense)
@@ -102,5 +114,39 @@ public class Quick{
 4-Complete Quicksort.
 5-Optionally implement dutch flag paritioning and update quicksort to include this.
 */
+public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
 
 }
